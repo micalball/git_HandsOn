5a98c3435f44fee49881ed46de01db7a43aeb230
MiquelAB <miquel.alberca@uvic.cat>
2024-02-18 22:54:51 +0100

    Changes to be committed: 	modified:   seqClass.py

--- seqClass.py
+++ seqClass.py
@@ -20,5 +20,5 @@ if re.search('^[ACGTU]+$', args.seq):
     else:
         print ('The sequence can be DNA or RNA')
 else:
-    print ('The sequence is not DNA nor RNA')
+    print ('The sequence is not DNA or RNA')
 
