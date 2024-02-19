4d221067057f135ec62bde729ff9d1858a9cf8d6
micalball <miquel.alberca@uvic.cat>
2024-02-19 02:41:41 +0100

    fixed_function

--- seqClass.py
+++ seqClass.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
-
+# First exercise, after creating fix branch we try to fix seqClass.py to see if it is able to classify correctly any RNA or DNA sequence.
 import sys, re
 from argparse import ArgumentParser
 
 parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA')
 parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence")
+#line added to identify motifs
 parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif")
 
 if len(sys.argv) == 1:
@@ -13,17 +14,22 @@ if len(sys.argv) == 1:
 
 args = parser.parse_args()
 
-args.seq = args.seq.upper()            
+args.seq = args.seq.upper() # Note we just added this line
 if re.search('^[ACGTU]+$', args.seq):
-    if re.search('T', args.seq):
-        print ('The sequence is DNA')
-    elif re.search('U', args.seq):
-        print ('The sequence is RNA')
+    # Add a condition so if the sequence contains both T and U it doesn't compute it as DNA
+    if 'T' in args.seq and 'U' in args.seq:
+        print('The sequence contains both T and U, and is ambiguous.')
+    elif 'T' in args.seq:
+        print('The sequence is DNA')
+    elif 'U' in args.seq:
+        print('The sequence is RNA')
     else:
-        print ('The sequence can be DNA or RNA')
+        # If the sequence doesn't contains 'T' or 'U', it could be DNA or RNA
+        print('The sequence can be DNA or RNA')
 else:
-    print ('The sequence is not DNA or RNA')
+    print('The sequence is not DNA or RNA')
 
+#code addition provided by teacher, where motif is searched in the sequence
 if args.motif:
     args.motif = args.motif.upper()
     print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
